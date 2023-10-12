e57ac18916a877ded8571e456c74a140665e2782
Jainil <jainiljp72525@gmail.com>
2023-10-12 11:44:49 +0530

    Branch dev

--- app.py
+++ app.py
@@ -7,27 +7,6 @@ cors = CORS(app)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/calculate_api', methods=['POST'])
-def calculate_api():
-
-    app.logger.info('This is an access log message.')
-    try: 
-        input = request.get_json()
-        print(input)
-        currentAge = int(input.get("currentAge"))
-        retirementAge = int(input.get("retirementAge"))
-        corpusGoal = float(input.get("corpusGoal"))
-        interestRate = float(input.get("interestRate"))
-        response = calculator(currentAge, retirementAge, corpusGoal, interestRate)
-        return response
-    except Exception as e:
-        #print("Post data reading error:",e) # Send error to server 
-        return {"error":403}
-    
 
 if __name__ == "__main__":
     app.run(port=5000)
